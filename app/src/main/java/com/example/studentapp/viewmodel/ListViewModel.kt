fd7905cd411f1b29177dfb97dd21e3bda6ca8e17
theodorusRH <s160421144@student.ubaya.ac.id>
2024-03-18 12:01:08 +0700

    week 4 update

--- app/src/main/java/com/example/studentapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/studentapp/viewmodel/ListViewModel.kt
@@ -1,24 +1,50 @@
 package com.example.studentapp.viewmodel
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.studentapp.model.Student
 
-class ListViewModel:ViewModel() {
+class ListViewModel(application: Application):AndroidViewModel(application) {
     var studentsLD = MutableLiveData<ArrayList<Student>>()
     val studentLoadErrorLD = MutableLiveData<Boolean>()
     val loadingLD = MutableLiveData<Boolean>()
+    val TAG = "volleyTag"
+    private var queue:RequestQueue? = null
 
     fun refresh() {
 
-        studentsLD.value = arrayListOf(
-            Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff"),
-            Student("13312","Rich","1994/12/14","3925444073","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff"),
-            Student("11204","Dinny","1994/10/07","6827808747","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff1"),
-        )
+//        studentsLD.value = arrayListOf(
+//            Student("16055","Nonie","1998/03/28","5718444778","http://dummyimage.com/75x100.jpg/cc0000/ffffff"),
+//            Student("13312","Rich","1994/12/14","3925444073","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff"),
+//            Student("11204","Dinny","1994/10/07","6827808747","http://dummyimage.com/75x100.jpg/5fa2dd/ffffff1"),
+//        )
 
         studentLoadErrorLD.value = false
         loadingLD.value = false
+
+        queue = Volley.newRequestQueue(getApplication())
+        val url = "http://adv.jitusolution.com/student.php"
+
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,{
+                loadingLD.value = false
+                Log.d("showvolley",it)
+            },{
+                loadingLD.value = false
+                studentLoadErrorLD.value=false
+                Log.d("showvolley",it.toString())
+            }
+        )
+        stringRequest.tag = TAG
+        queue?.add(stringRequest)
+
     }
 
 }
